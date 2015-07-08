7ece31ef1ee3982ea2de6e97ff2dc75e65934dea
Acey <acey1@users.noreply.github.com>
2015-07-08 06:47:18 +0530

    Update gridcoin.rb
    Latest grc vestion 3.4.2.6 tested and working on my fork.

--- gridcoin.rb
+++ gridcoin.rb
@@ -1,7 +1,7 @@
 class Gridcoin < Formula
   desc "GridCoin OS X client (GUI and CLI)"
   homepage "http://gridcoin.us"
-  head "https://github.com/gridcoin/Gridcoin-Research.git", :revision => 'e20e1964356fdbe9'
+  head "https://github.com/gridcoin/Gridcoin-Research.git", :revision => '92c5620a8b134788'
 
   option "with-cli", "Also compile the command line client"
   option "without-gui", "Do not compile the graphical client"
