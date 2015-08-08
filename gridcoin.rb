0d18b67410db25bdcbd1ce09ed692e700b9d1c49
Martin Schimandl <martin.schimandl@gmail.com>
2015-08-08 07:42:14 +0200

    Bump version to 3.4.6.8

--- gridcoin.rb
+++ gridcoin.rb
@@ -1,7 +1,7 @@
 class Gridcoin < Formula
   desc "GridCoin OS X client (GUI and CLI)"
   homepage "http://gridcoin.us"
-  head "https://github.com/gridcoin/Gridcoin-Research.git", :revision => '78a5a2c723975'
+  head "https://github.com/gridcoin/Gridcoin-Research.git", :revision => '5693f7e6a0f01b03a'
 
   option "with-cli", "Also compile the command line client"
   option "without-gui", "Do not compile the graphical client"
