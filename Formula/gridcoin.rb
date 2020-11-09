4a1eff029c86ecc05dda223f9c00db7e219c2ec4
scribblemaniac <scribblemaniac@users.noreply.github.com>
2020-11-08 19:09:27 -0700

    Add basic test block implementation

--- Formula/gridcoin.rb
+++ Formula/gridcoin.rb
@@ -84,15 +84,9 @@ class Gridcoin < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test Gridcoin`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system bin/"gridcoinresearchd", "-version" if build.with? "cli"
+
+    # Currently help is the only flag which does not actually start the gui
+    system prefix/"gridcoinresearch.app/Contents/MacOS/gridcoinresearch", "-?" if build.with? "gui"
   end
 end
